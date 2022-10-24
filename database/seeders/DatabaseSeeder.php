cd5deb1090575c6d0c90148baeb1bea2461b6017
Bokele Wakiza Franck <bwakiza@gmail.com>
2022-10-24 22:21:02 +0200

    adding seed for user and role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +17,28 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Loan Admin',
+            'email' => 'admin@fnb.loan.co.zm',
+            'password' => bcrypt('password'),
+            'job_type' => 'full time',
+            'base_salary' => 20000
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Loan Staff',
+            'email' => 'staff@fnb.loan.co.zm',
+            'password' => bcrypt('password'),
+            'job_type' => 'full time',
+            'base_salary' => 20000
+        ]);
+
+        Role::factory()->create([
+            'name' => 'admin'
+        ]);
+        Role::factory()->create([
+            'name' => 'staff'
+        ]);
     }
 }
